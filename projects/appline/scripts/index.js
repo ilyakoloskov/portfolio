1b8e3c13b1123f725f1875baf1a4dc13d2991df8
mac <mac@MacBook-Pro-Kolos.local>
2024-05-19 13:30:12 +0300

    #Add first layout appline

---
+++ projects/appline/scripts/index.js
@@ -0,0 +1,25 @@
+const servicesSlider = new Swiper(".swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const portoflioSlider = new Swiper(".works__slider", {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  autoplay: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
